6b1b945983935bdd5edd70a755f4e09866892362
shu fujita <momo.8jack@gmail.com>
2020-05-13 02:42:17 +0900

    Sound Challenge

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -12,8 +12,12 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
 
     let drumSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name:"drumSound")!.data)
+    let pianoSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name:"pianoSound")!.data)
+    let guitarSoundPlayer = try! AVAudioPlayer(data:NSDataAsset(name:"guitarSound")!.data)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +30,25 @@ class ViewController: UIViewController {
         drumSoundPlayer.play()
     }
     @IBAction func touchUpDrumButton(){
-        
         drumButton.setImage(UIImage(named: "drumImage"), for: .normal)
     }
-}
+    
+    @IBAction func touchDownPianoButton(){
+       pianoButton.setImage(UIImage(named:"pianoPlayingImage"), for: .normal)
+       pianoSoundPlayer.currentTime = 0
+       pianoSoundPlayer.play()
+    }
+    @IBAction func touchUpPianoButton(){
+        pianoButton.setImage(UIImage(named: "pianoImage"), for: .normal)
+    }
+    
+    @IBAction func touchDownGuitarButton(){
+        guitarButton.setImage(UIImage(named:"guitarPlayingImage"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+    }
+    @IBAction func touchUpGuitarButton(){
+        guitarButton.setImage(UIImage(named: "guitarImage"), for: .normal)
+    }
+    }
+
