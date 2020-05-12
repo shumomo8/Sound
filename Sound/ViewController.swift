ba5e1069636972e92cf4d6061b59d7f0e2346122
shu fujita <momo.8jack@gmail.com>
2020-05-12 12:07:08 +0900

    Initial Commit

---
+++ Sound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sound
+//
+//  Created by Shu Fujita on 2020/05/12.
+//  Copyright © 2020 Fujita shu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
